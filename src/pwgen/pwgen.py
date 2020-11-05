8e952a4845d636017a10b2740030f4f2bc12a2d0
Amuzak, I <sennuma@gmail.com>
2020-11-05 15:29:18 +0900

    update

--- src/pwgen/pwgen.py
+++ src/pwgen/pwgen.py
@@ -1,5 +1,40 @@
-def genpassword(ID: str = None, l: int = 8, punc: bool = True) -> tuple:
+#
+#
+
+pwspath: str = ".\\json\\pws\\pws.json"
+
+
+def initialize(path: str) -> dict:
     """"""
+    from json import load
+
+    with open(path, encoding="utf8") as f:
+        d: dict = load(f)
+    return d
+
+
+def finalize(path: str, obj: object) -> None:
+    """"""
+    from json import dump
+
+    with open(path, mode="w", encoding="utf8") as f:
+        dump(obj=obj, fp=f, ensure_ascii=False, indent=4)
+
+
+def genpassword(ID: str = None, l: int = 8, punc: bool = True) -> dict:
+    """
+    Generate password under given conditions.
+    Params
+    --------
+    ID: str
+    l: int
+    punc: bool
+    Return
+    --------
+    tuple[ID, generated_pw]
+    ID: str
+    generated_pw: str
+    """
     chars: list = []
     from random import choice
     from string import ascii_letters, digits
@@ -10,4 +45,7 @@ def genpassword(ID: str = None, l: int = 8, punc: bool = True) -> tuple:
 
         chars.extend(punctuation)
 
-    return (ID, "".join([choice(chars) for i in range(l)]))
+    return {ID: "".join([choice(chars) for i in range(l)])}
+
+
+d = initialize(pwspath)
